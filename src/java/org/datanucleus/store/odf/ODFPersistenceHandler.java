78b8c2f286791135ea022d59361482aa596d19c3
andyjefferson <andy@datanucleus.org>
2014-04-18 09:42:04 +0100

    javadoc

--- src/java/org/datanucleus/store/odf/ODFPersistenceHandler.java
+++ src/java/org/datanucleus/store/odf/ODFPersistenceHandler.java
@@ -50,7 +50,7 @@ import org.odftoolkit.odfdom.dom.attribute.office.OfficeValueTypeAttribute;
  * Persistence Handler for Open Document Format (ODF) datastores.
  * Handles the insert/update/delete/fetch/locate operations by using ODF Toolkit.
  * <p>
- * <b>Field -> Cell mapping</b>
+ * <b>Field to Cell mapping</b>
  * A field is mapped to a cell. The field metadata can define a cell number (starting at 0).
  * Specifying the cell number means that the user takes responsibility for the cell numbers
  * being consistent. The default cell numbering is alphabetical start in the root class, and working
